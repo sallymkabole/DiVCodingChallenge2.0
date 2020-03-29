54f6fce9f4683636744427e719f264d620bad842
sally <you@example.com>
2020-03-29 22:03:16 +0000

    Add Tasks component to display list of tasks to be done

---
+++ client/src/components/Tasks.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container">
+          <div class="container">
+    <div class="row">
+      <div class="col-sm-10">
+        <h1>Tasks</h1>
+        <hr><br><br>
+        <button type="button" class="btn btn-success btn-sm">Add Task</button>
+        <br><br>
+        <table class="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Severity Rating</th>
+              <th scope="col">Start Date</th>
+              <th scope="col">End Date</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Draft Digital Compaign</td>
+              <td>5</td>
+              <td>12/03/2020</td>
+              <td>
+                <div class="btn-group" role="group">
+                  <button type="button" class="btn btn-warning btn-sm">Update</button>
+                  <button type="button" class="btn btn-danger btn-sm">Delete</button>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>User Reserch</td>
+              <td>2</td>
+              <td>12/03/2020</td>
+              <td>
+                <div class="btn-group" role="group">
+                  <button type="button" class="btn btn-warning btn-sm">Update</button>
+                  <button type="button" class="btn btn-danger btn-sm">Delete</button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+    </div>
+</template>
--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Ping from './components/Ping.vue';
+import Tasks from './components/Tasks.vue';
 
 Vue.use(Router);
 
@@ -8,6 +9,11 @@ export default new Router({
   mode: 'history', // enable page load without reload
   base: process.env.BASE_URL,
   routes: [
+    {
+      path: '/tasks',
+      name: '/Tasks',
+      component: Tasks,
+    },
     {
       path: '/ping',
       name: 'Ping',
